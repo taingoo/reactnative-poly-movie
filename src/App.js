a2e18f3dc7826e0cb7f17b1c9e5760d3b9c654fd
Tai Ngo <tantai23493@gmail.com>
2020-07-26 18:10:38 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import RootStack from './core/navigation/RootStack';
+
+export default function App() {
+  return <RootStack></RootStack>;
+}
