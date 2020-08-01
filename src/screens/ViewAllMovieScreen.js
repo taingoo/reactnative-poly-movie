bae5a471b41bfe1ad6e7899786ae887c14b1b6c3
Tai Ngo <tantai23493@gmail.com>
2020-08-01 07:21:24 +0700

    fix code ViewAllScreen

--- src/screens/ViewAllMovieScreen.js
+++ src/screens/ViewAllMovieScreen.js
@@ -4,156 +4,55 @@ import axiosConfig from '../api/axios';
 import MovieItem from '../components/List/MovieItem';
 import NavBar from '../components/NavBar';
 
+const TAGs = {
+  popular: 'popular',
+  nowplaying: 'now_playing',
+  upcoming: 'upcoming',
+  top_rated: 'top_rated',
+};
+
 export default function ViewAllMovieScreen({route}) {
   const [data, setData] = useState({});
-  const [popular, setPopular] = useState([]);
-  const [nowplaying, setNowPlaying] = useState([]);
-  const [upcoming, setUpComing] = useState([]);
-  const [toprated, setTopRated] = useState([]);
+  const [movie, setMovie] = useState([]);
   const [page, setPage] = useState(1);
-  let numColumn = 3;
+  const tag = route.params.tag;
+  const numColumn = 3;
 
   useEffect(() => {
-    if (`${route.params.tag}` === 'popular') {
-      axiosConfig
-        .get('/movie/popular', {
-          params: {
-            api_key: 'cfb5e7441170e569be1265dadbb2df82',
-            page: page,
-          },
-        })
-        .then((response) => {
-          setData(response.data);
-          setPopular(response.data.results);
-        });
-    } else if (`${route.params.tag}` === 'nowplaying') {
-      axiosConfig
-        .get('movie/now_playing', {
-          params: {
-            api_key: 'cfb5e7441170e569be1265dadbb2df82',
-            page: page,
-          },
-        })
-        .then((response) => {
-          setData(response.data);
-          setNowPlaying(response.data.results);
-        });
-    } else if (`${route.params.tag}` === 'upcoming') {
-      axiosConfig
-        .get('/movie/upcoming', {
-          params: {
-            api_key: 'cfb5e7441170e569be1265dadbb2df82',
-            page: page,
-          },
-        })
-        .then((response) => {
-          setData(response.data);
-          setUpComing(response.data.results);
-        });
-    } else {
-      axiosConfig
-        .get('/movie/top_rated', {
-          params: {
-            api_key: 'cfb5e7441170e569be1265dadbb2df82',
-            page: page,
-          },
-        })
-        .then((response) => {
-          setData(response.data);
-          setTopRated(response.data.results);
-        });
-    }
-  }, [page, route.params.tag]);
+    axiosConfig
+      .get(`/movie/${TAGs[tag]}`, {
+        params: {
+          api_key: 'cfb5e7441170e569be1265dadbb2df82',
+          page: page,
+        },
+      })
+      .then((response) => {
+        setData(response.data);
+        setMovie((oldMovie) => oldMovie.concat(response.data.results));
+      });
+  }, [page, tag]);
 
-  if (`${route.params.tag}` === 'popular') {
-    return (
-      <View style={styles.container}>
-        <NavBar page={page} total={data.total_pages} backTo="Main" />
-        <View style={{paddingHorizontal: 5}}>
-          <FlatList
-            numColumns={numColumn}
-            data={popular}
-            keyExtractor={(item, index) => index.toString()}
-            renderItem={({item}) => (
-              <MovieItem
-                goTo="DetailMovie"
-                id={item.id}
-                image={item.poster_path}
-              />
-            )}
-            onEndReached={() => setPage(page + 1)}
-            onEndReachedThreshold={0.1}
-          />
-        </View>
+  return (
+    <View style={styles.container}>
+      <NavBar page={page} total={data.total_pages} backTo="Main" />
+      <View style={{paddingHorizontal: 5}}>
+        <FlatList
+          numColumns={numColumn}
+          data={movie}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={({item}) => (
+            <MovieItem
+              goTo="DetailMovie"
+              id={item.id}
+              image={item.poster_path}
+            />
+          )}
+          onEndReached={() => setPage(page + 1)}
+          onEndReachedThreshold={0.1}
+        />
       </View>
-    );
-  } else if (`${route.params.tag}` === 'nowplaying') {
-    return (
-      <View style={styles.container}>
-        <NavBar page={page} total={data.total_pages} backTo="Main" />
-        <View style={{paddingHorizontal: 5}}>
-          <FlatList
-            numColumns={numColumn}
-            data={nowplaying}
-            keyExtractor={(item, index) => index.toString()}
-            renderItem={({item}) => (
-              <MovieItem
-                goTo="DetailMovie"
-                id={item.id}
-                image={item.poster_path}
-              />
-            )}
-            onEndReached={() => setPage(page + 1)}
-            onEndReachedThreshold={0.1}
-          />
-        </View>
-      </View>
-    );
-  } else if (`${route.params.tag}` === 'upcoming') {
-    return (
-      <View style={styles.container}>
-        <NavBar page={page} total={data.total_pages} backTo="Main" />
-        <View style={{paddingHorizontal: 5}}>
-          <FlatList
-            numColumns={numColumn}
-            data={upcoming}
-            keyExtractor={(item, index) => index.toString()}
-            renderItem={({item}) => (
-              <MovieItem
-                goTo="DetailMovie"
-                id={item.id}
-                image={item.poster_path}
-              />
-            )}
-            onEndReached={() => setPage(page + 1)}
-            onEndReachedThreshold={0.1}
-          />
-        </View>
-      </View>
-    );
-  } else {
-    return (
-      <View style={styles.container}>
-        <NavBar page={page} total={data.total_pages} backTo="Main" />
-        <View style={{paddingHorizontal: 5}}>
-          <FlatList
-            numColumns={numColumn}
-            data={toprated}
-            keyExtractor={(item, index) => index.toString()}
-            renderItem={({item}) => (
-              <MovieItem
-                goTo="DetailMovie"
-                id={item.id}
-                image={item.poster_path}
-              />
-            )}
-            onEndReached={() => setPage(page + 1)}
-            onEndReachedThreshold={0.1}
-          />
-        </View>
-      </View>
-    );
-  }
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
--- src/screens/ViewAllTVScreen.js
+++ src/screens/ViewAllTVScreen.js
@@ -4,151 +4,49 @@ import axiosConfig from '../api/axios';
 import TVItem from '../components/List/TVItem';
 import NavBar from '../components/NavBar';
 
+const TAGs = {
+  popular: 'popular',
+  airing: 'airing_today',
+  ontv: 'on_the_air',
+  top_rated: 'top_rated',
+};
+
 export default function ViewAllTVScreen({route}) {
   const [data, setData] = useState({});
-  const [popular, setPopular] = useState([]);
-  const [nowplaying, setNowPlaying] = useState([]);
-  const [upcoming, setUpComing] = useState([]);
-  const [toprated, setTopRated] = useState([]);
+  const [movie, setMovie] = useState([]);
   const [page, setPage] = useState(1);
+  const tag = route.params.tag;
 
   useEffect(() => {
-    if (`${route.params.tag}` === 'popular') {
-      axiosConfig
-        .get('/tv/popular', {
-          params: {
-            api_key: 'cfb5e7441170e569be1265dadbb2df82',
-            page: page,
-          },
-        })
-        .then((response) => {
-          setData(response.data);
-          setPopular(response.data.results);
-        });
-    } else if (`${route.params.tag}` === 'airing') {
-      axiosConfig
-        .get('/tv/airing_today', {
-          params: {
-            api_key: 'cfb5e7441170e569be1265dadbb2df82',
-            page: page,
-          },
-        })
-        .then((response) => {
-          setData(response.data);
-          setNowPlaying(response.data.results);
-        });
-    } else if (`${route.params.tag}` === 'ontv') {
-      axiosConfig
-        .get('/tv/on_the_air', {
-          params: {
-            api_key: 'cfb5e7441170e569be1265dadbb2df82',
-            page: page,
-          },
-        })
-        .then((response) => {
-          setData(response.data);
-          setUpComing(response.data.results);
-        });
-    } else {
-      axiosConfig
-        .get('/movie/top_rated', {
-          params: {
-            api_key: 'cfb5e7441170e569be1265dadbb2df82',
-            page: {page},
-          },
-        })
-        .then((response) => {
-          setData(response.data);
-          setTopRated(response.data.results);
-        });
-    }
-  }, [page, route.params.tag]);
+    axiosConfig
+      .get(`/tv/${TAGs[tag]}`, {
+        params: {
+          api_key: 'cfb5e7441170e569be1265dadbb2df82',
+          page: page,
+        },
+      })
+      .then((response) => {
+        setData(response.data);
+        setMovie((oldTV) => oldTV.concat(response.data.results));
+      });
+  }, [page, tag]);
 
-  if (`${route.params.tag}` === 'popular') {
-    return (
-      <View style={styles.container}>
-        <NavBar page={page} total={data.total_pages} backTo="Main" />
-        <View style={{paddingHorizontal: 5}}>
-          <FlatList
-            data={popular}
-            keyExtractor={(item, index) => index.toString()}
-            renderItem={({item}) => (
-              <TVItem
-                goTo="DetailMovie"
-                id={item.id}
-                image={item.poster_path}
-              />
-            )}
-            onEndReached={() => setPage(page + 1)}
-            onEndReachedThreshold={0.1}
-          />
-        </View>
+  return (
+    <View style={styles.container}>
+      <NavBar page={page} total={data.total_pages} backTo="Main" />
+      <View style={{paddingHorizontal: 5}}>
+        <FlatList
+          data={movie}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={({item}) => (
+            <TVItem goTo="DetailMovie" id={item.id} image={item.poster_path} />
+          )}
+          onEndReached={() => setPage(page + 1)}
+          onEndReachedThreshold={0.1}
+        />
       </View>
-    );
-  } else if (`${route.params.tag}` === 'airing') {
-    return (
-      <View style={styles.container}>
-        <NavBar page={page} total={data.total_pages} backTo="Main" />
-        <View style={{paddingHorizontal: 5}}>
-          <FlatList
-            data={nowplaying}
-            keyExtractor={(item, index) => index.toString()}
-            renderItem={({item}) => (
-              <TVItem
-                goTo="DetailMovie"
-                id={item.id}
-                image={item.poster_path}
-              />
-            )}
-            onEndReached={() => setPage(page + 1)}
-            onEndReachedThreshold={0.1}
-          />
-        </View>
-      </View>
-    );
-  } else if (`${route.params.tag}` === 'ontv') {
-    return (
-      <View style={styles.container}>
-        <NavBar page={page} total={data.total_pages} backTo="Main" />
-        <View style={{paddingHorizontal: 5}}>
-          <FlatList
-            data={upcoming}
-            keyExtractor={(item, index) => index.toString()}
-            renderItem={({item}) => (
-              <TVItem
-                goTo="DetailMovie"
-                id={item.id}
-                image={item.poster_path}
-              />
-            )}
-            onEndReached={() => setPage(page + 1)}
-            onEndReachedThreshold={0.1}
-          />
-        </View>
-      </View>
-    );
-  } else {
-    return (
-      <View style={styles.container}>
-        <NavBar page={page} total={data.total_pages} backTo="Main" />
-        <View style={{paddingHorizontal: 5}}>
-          <FlatList
-            data={toprated}
-            keyExtractor={(item, index) => index.toString()}
-            renderItem={({item}) => (
-              <TVItem
-                goTo="DetailMovie"
-                id={item.id}
-                image={item.poster_path}
-              />
-            )}
-            onEndReached={() => setPage(page + 1)}
-            onEndReachedThreshold={0.1}
-          />
-        </View>
-      </View>
-    );
-  }
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
