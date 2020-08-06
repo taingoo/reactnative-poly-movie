ce30432ddffd70791ab9954ae9411082758dd85a
Tai Ngo <tantai23493@gmail.com>
2020-08-07 04:42:07 +0700

    fix SplashScren issue on Android

--- android/app/src/main/java/com/polymovie/MainActivity.java
+++ android/app/src/main/java/com/polymovie/MainActivity.java
@@ -1,23 +1,29 @@
 package com.polymovie;
 
-import com.facebook.react.ReactActivity;
-import org.devio.rn.splashscreen.SplashScreen;
 import android.os.Bundle;
+import android.os.Handler;
+
+import com.facebook.react.ReactActivity;
+
+import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
-     // Add this method.
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    SplashScreen.show(this);
-    super.onCreate(savedInstanceState);
-  }
-  @Override
-  protected String getMainComponentName() {
-    return "PolyMovie";
-  }
+    /**
+     * Returns the name of the main component registered from JavaScript. This is used to schedule
+     * rendering of the component.
+     */
+    // Add this method.
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(MainActivity.this);
+        super.onCreate(savedInstanceState);
+
+        new Handler().postDelayed(() -> SplashScreen.hide(MainActivity.this), 1500);
+    }
+
+    @Override
+    protected String getMainComponentName() {
+        return "PolyMovie";
+    }
 }
